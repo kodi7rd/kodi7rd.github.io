267823a8ca5b412401fe92eef30adbc23887556a
kodi7rd <KodiRealDebridIsrael@github.com>
2023-02-11 02:39:49 +0200

    Update addons.db with wizard installed from repo

--- builds/build20_zip/Kodi20-RealDebrid-Israel-Build-3.0.2/addons/service.subtitles.All_Subs_Flash/auto_translate/googletrans/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Free Google Translate API for Python. Translates totally free of charge."""
-__all__ = 'Translator',
-__version__ = '3.1.0-alpha'
-
-
-from auto_translate.googletrans.client import Translator
-from auto_translate.googletrans.constants import LANGCODES, LANGUAGES  # noqa
