829ab728b786eae30f4f7c716b6bd256406f6419
kodi7rd <KodiRealDebridIsrael@github.com>
2023-09-02 11:59:00 +0300

    Rearrange dirs

--- builds/build20_skin_FENtastic_TEST/build20_skin_FENtastic/addons/service.subtitles.All_Subs/resources/modules/auto_translate/httpcore/_ssl.py
+++
@@ -1,8 +0,0 @@
-import certifi
-import ssl
-
-
-def default_ssl_context() -> ssl.SSLContext:
-    context = ssl.create_default_context()
-    context.load_verify_locations(certifi.where())
-    return context
