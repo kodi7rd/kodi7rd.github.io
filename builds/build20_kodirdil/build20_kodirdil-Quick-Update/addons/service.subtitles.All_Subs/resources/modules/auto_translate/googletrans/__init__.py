2de678a644705aaa61bdc2e6fa33e05a89d88940
kodi7rd <KodiRealDebridIsrael@github.com>
2024-01-06 21:21:47 +0200

    KODI-RD-IL QU | new design estuary - my movies/series + in progress

--- builds/build20_kodirdil/build20_kodirdil-Quick-Update/addons/service.subtitles.All_Subs/resources/modules/auto_translate/googletrans/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Free Google Translate API for Python. Translates totally free of charge."""
-__all__ = 'Translator',
-__version__ = '3.1.0-alpha'
-
-
-from resources.modules.auto_translate.googletrans.client import Translator
-from resources.modules.auto_translate.googletrans.constants import LANGCODES, LANGUAGES  # noqa
