c8fff2d48ddc329c822ac61a787c2ba29c582a3b
kodi7rd <KodiRealDebridIsrael@github.com>
2024-02-18 00:08:07 +0200

    KODI-RD-IL | CocoScrapers | 1.0.18

--- builds/build20_kodirdil/build20_kodirdil/addons/script.module.cocoscrapers/lib/cocoscrapers/sources_cocoscrapers/torrents/eztv.py
+++ builds/build20_kodirdil/build20_kodirdil/addons/script.module.cocoscrapers/lib/cocoscrapers/sources_cocoscrapers/torrents/eztv.py
@@ -1,34 +1,26 @@
 # -*- coding: utf-8 -*-
-# modified by CocoJoe for CocoScrapers (updated 12-12-2023)
+# modified by Venom for Fenomscrapers (updated 7-19-2022)
 """
-	CocoScrapers Project
+	Fenomscrapers Project
 """
 
-from math import ceil
-from json import loads as jsloads
 import re
 from urllib.parse import quote_plus, unquote_plus
 from cocoscrapers.modules import client
 from cocoscrapers.modules import source_utils
-from cocoscrapers.modules import workers
+
 
 class source:
 	priority = 6
-	pack_capable = False
+	pack_capable = True
 	hasMovies = False
 	hasEpisodes = True
 	def __init__(self):
 		self.language = ['en']
-		self.base_link = 'https://eztvx.to/api/get-torrents?imdb_id=%s&limit=100&page=%s'
+		self.base_link = "https://eztvx.to/"
+		# eztv has api but it sucks. Site query returns more results vs. api (eztv db seems to be missing the imdb_id for many so they are dropped)
+		self.search_link = '/search/%s'
 		self.min_seeders = 0
-		self.results = []
-
-	def get_extra_pages(self, url):
-		result = client.request(url, timeout=5)
-		try:
-			files = jsloads(result)['torrents']
-			self.results.extend(files)
-		except: return
 
 	def sources(self, data, hostDict):
 		sources = []
@@ -39,65 +31,136 @@ class source:
 			aliases = data['aliases']
 			episode_title = data['title']
 			year = data['year']
-			imdb = data['imdb'].replace('tt', '')
-			season = data['season']
-			episode = data['episode']
-			hdlr = 'S%02dE%02d' % (int(season), int(episode))
-
-			base_url = self.base_link % (imdb, '%s')
-
-			result = client.request(base_url % 1, timeout=5)
-			try: result = jsloads(result)
-			except: return sources
-			self.results.extend(result['torrents'])
-
-			total_pages = ceil(result['torrents_count']/float(result['limit']))
-			if total_pages > 1:
-				threads = []
-				append = threads.append
-				all_pages = range(2, total_pages + 1)
-				for page_no in all_pages:
-					append(workers.Thread(self.get_extra_pages, base_url % page_no))
-				[i.start() for i in threads]
-				[i.join() for i in threads]
-			
+			hdlr = 'S%02dE%02d' % (int(data['season']), int(data['episode']))
+			query = '%s %s' % (re.sub(r'[^A-Za-z0-9\s\.]+', '', title), hdlr) # eztv has issues with dashes in titles
+			url = self.search_link % quote_plus(query).replace('+', '-')
+			url = '%s%s' % (self.base_link, url)
+			#from cocoscrapers.modules import log_utils
+			#log_utils.log('url = %s' % url)
+			results = client.request(url, timeout=5)
+			if not results: return sources
+			rows = client.parseDOM(results, 'tr')
+			if not rows: return sources
 			undesirables = source_utils.get_undesirables()
 			check_foreign_audio = source_utils.check_foreign_audio()
-		
 		except:
 			source_utils.scraper_error('EZTV')
 			return sources
 
-		for file in self.results:
+		for row in rows:
 			try:
-				hash = file['hash']
+				if 'magnet:' not in row: continue
+				try:
+					columns = re.findall(r'<td\s.+?>(.*?)</td>', row, re.DOTALL)
+					link = re.findall(r'href\s*=\s*["\'](magnet:[^"\']+)["\'].*?title\s*=\s*["\'](.+?)["\']', columns[2], re.DOTALL | re.I)[0]
+				except: continue
+
+				url = unquote_plus(client.replaceHTMLCodes(link[0])).split('&tr')[0]
+				hash = re.search(r'btih:(.*?)(?:&|$)', url, re.I).group(1)
 				if len(hash) != 40: # eztv has some base32 encoded hashes
+					from cocoscrapers.modules import log_utils
 					hash = source_utils.base32_to_hex(hash, 'EZTV')
-				
-				name = file['filename']
+					log_utils.log('url with base32 hash:  "%s" ' % url, __name__, log_utils.LOGDEBUG)
+					url = re.sub(re.search(r'btih:(.*?)(?:&|$)', url).group(1), hash, url)
+					log_utils.log('url converted to hex40 hash:  "%s" ' % url, __name__, log_utils.LOGDEBUG)
+
+				name = ''.join(link[1].partition('[eztv]')[:2]).replace(' Torrent: Magnet Link', '')
 				name = source_utils.clean_name(name)
-				
-				url = 'magnet:?xt=urn:btih:%s&dn=%s' % (hash, name)
 
 				if not source_utils.check_title(title, aliases, name, hdlr, year): continue
 				name_info = source_utils.info_from_name(name, title, year, hdlr, episode_title)
 				if source_utils.remove_lang(name_info, check_foreign_audio): continue
 				if undesirables and source_utils.remove_undesirables(name_info, undesirables): continue
 
-				seeders = file['seeds']
+				try:
+					seeders = int(re.search(r'>(\d+|\d+\,\d+)<', columns[5]).group(1).replace(',', ''))
+					if self.min_seeders > seeders: continue
+				except: seeders = 0
 
 				quality, info = source_utils.get_release_quality(name_info, url)
-
-				size = '%sgb' % round(float(file['size_bytes'])/1073741824, 2)
 				try:
-					dsize, isize = source_utils._size(size)
+					dsize, isize = source_utils._size(columns[3])
 					info.insert(0, isize)
 				except: dsize = 0
 				info = ' | '.join(info)
-				
+
 				sources_append({'provider': 'eztv', 'source': 'torrent', 'seeders': seeders, 'hash': hash, 'name': name, 'name_info': name_info,
 								'quality': quality, 'language': 'en', 'url': url, 'info': info, 'direct': False, 'debridonly': True, 'size': dsize})
-			
+			except:
+				source_utils.scraper_error('EZTV')
+		return sources
+
+	def sources_packs(self, data, hostDict, search_series=False, total_seasons=None, bypass_filter=False):
+		sources = []
+		if search_series: return sources # eztz does not have showPacks
+		if not data: return sources
+		sources_append = sources.append
+		try:
+			title = data['tvshowtitle'].replace('&', 'and').replace('Special Victims Unit', 'SVU').replace('/', ' ').replace('$', 's')
+			aliases = data['aliases']
+			year = data['year']
+			season_x = data['season']
+			season_xx = season_x.zfill(2)
+
+			# query = re.sub(r'[^A-Za-z0-9\s\.-]+', '', title) #eztv has issues with dashes in titles
+			query = re.sub(r'[^A-Za-z0-9\s\.]+', '', title)
+			url = self.search_link % quote_plus(query + ' S%s' % season_xx).replace('+', '-')
+			url = '%s%s' % (self.base_link, url)
+			results = client.request(url, timeout=5)
+			if not results: return
+			rows = client.parseDOM(results, 'tr')
+			if not rows: return
+			undesirables = source_utils.get_undesirables()
+			check_foreign_audio = source_utils.check_foreign_audio()
+		except:
+			source_utils.scraper_error('EZTV')
+			return sources
+
+		for row in rows:
+			try:
+				if 'magnet:' not in row: continue
+				try:
+					columns = re.findall(r'<td\s.+?>(.*?)</td>', row, re.DOTALL)
+					link = re.findall(r'href\s*=\s*["\'](magnet:[^"\']+)["\'].*?title\s*=\s*["\'](.+?)["\']', columns[2], re.DOTALL | re.I)[0]
+				except: continue
+
+				url = unquote_plus(client.replaceHTMLCodes(link[0])).split('&tr')[0]
+				hash = re.search(r'btih:(.*?)(?:&|$)', url, re.I).group(1)
+				if len(hash) != 40: # eztv has some base32 encoded hashes
+					from cocoscrapers.modules import log_utils
+					hash = source_utils.base32_to_hex(hash, 'EZTV')
+					log_utils.log('url with base32 hash:  "%s" ' % url, __name__, log_utils.LOGDEBUG)
+					url = re.sub(re.search(r'btih:(.*?)(?:&|$)', url).group(1), hash, url)
+					log_utils.log('url converted to hex40 hash:  "%s" ' % url, __name__, log_utils.LOGDEBUG)
+
+				name = ''.join(link[1].partition('[eztv]')[:2]).replace(' Torrent: Magnet Link', '')
+				name = source_utils.clean_name(name)
+
+				episode_start, episode_end = 0, 0
+				if not bypass_filter:
+					valid, episode_start, episode_end = source_utils.filter_season_pack(title, aliases, year, season_x, name)
+					if not valid: continue
+				package = 'season'
+
+				name_info = source_utils.info_from_name(name, title, year, season=season_x, pack=package)
+				if source_utils.remove_lang(name_info, check_foreign_audio): continue
+				if undesirables and source_utils.remove_undesirables(name_info, undesirables): continue
+				try:
+					seeders = int(re.search(r'>(\d+|\d+\,\d+)<', columns[5]).group(1).replace(',', ''))
+					if self.min_seeders > seeders: continue
+				except: seeders = 0
+
+				quality, info = source_utils.get_release_quality(name_info, url)
+				try:
+					dsize, isize = source_utils._size(columns[3])
+					info.insert(0, isize)
+				except: dsize = 0
+				info = ' | '.join(info)
+
+				item = {'provider': 'eztv', 'source': 'torrent', 'seeders': seeders, 'hash': hash, 'name': name, 'name_info': name_info, 'quality': quality,
+							'language': 'en', 'url': url, 'info': info, 'direct': False, 'debridonly': True, 'size': dsize, 'package': package}
+				if episode_start: item.update({'episode_start': episode_start, 'episode_end': episode_end}) # for partial season packs
+				sources_append(item)
 			except:
 				source_utils.scraper_error('EZTV')
 		return sources
--- builds/build20_kodirdil/build20_kodirdil/addons/script.module.cocoscrapers/lib/cocoscrapers/sources_cocoscrapers/torrents/rutor.py
+++ builds/build20_kodirdil/build20_kodirdil/addons/script.module.cocoscrapers/lib/cocoscrapers/sources_cocoscrapers/torrents/rutor.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# created by Venom for Fenomscrapers (1-18-2024)
+# created by Venom for Fenomscrapers (2-8-2024)
 """
-	Fenomscrapers Project
+	CocoScrapers Project
 """
 
 import re
@@ -14,7 +14,7 @@ class source:
 	priority = 3
 	pack_capable = True
 	hasMovies = True
-	hasEpisodes = True
+	hasEpisodes = False
 	def __init__(self):
 		self.language = ['en']
 		self.base_link = 'https://rutor.info'
@@ -23,9 +23,11 @@ class source:
 # 3rd param = 
 # 4th param = sort by. Date Added: increasing=1, Date Added: decreasing=0, Distributing: increasing=3, Distributing: decreasing=2, 
 # Rocking: increasing=5, Rocking: decreasing=4, Name: increasing=7, Name: decreasing=6, Size: increasing=9, Size: decreasing=8, Relevance: increasing=11, Relevance: decreasing=10
-#Foreign Movie = 0/1/110/8
-#Foreign TV = 0/4/110/8
+# Foreign Movie = 0/1/110/8
+# Foreign TV = 0/4/110/8
 		self.search_link = '/search/%s/%s'
+		self.movie_params = '0/0/0/8'
+		self.tvshow_params = '0/0/0/8'
 		self.min_seeders = 0
 
 	def sources(self, data, hostDict):
@@ -36,18 +38,19 @@ class source:
 			aliases = data['aliases']
 			year = data['year']
 			if 'tvshowtitle' in data:
-				title = data['tvshowtitle'].replace('&', 'and').replace('Special Victims Unit', 'SVU').replace('/', ' ').replace('$', 's')
-				episode_title = data['title']
-				hdlr = 'S%02dE%02d' % (int(data['season']), int(data['episode']))
-				url_insert = '0/4/110/8'
+				return sources
+				# title = data['tvshowtitle'].replace('&', 'and').replace('Special Victims Unit', 'SVU').replace('/', ' ').replace('$', 's')
+				# episode_title = data['title']
+				# hdlr = 'S%02dE%02d' % (int(data['season']), int(data['episode']))
+				# search_link = self.search_link % (self.tvshow_params, '%s')
 			else:
 				title = data['title'].replace('&', 'and').replace('/', ' ').replace('$', 's')
 				episode_title = None
 				hdlr = year
-				url_insert = '0/1/110/8'
+				search_link = self.search_link % (self.movie_params, '%s')
 			query = '%s %s' % (re.sub(r'[^A-Za-z0-9\s\.-]+', '', title), hdlr)
-			url = '%s%s' % (self.base_link, self.search_link % (url_insert, quote_plus(query)))
-			# log_utils.log('url = %s' % url)
+			url = '%s%s' % (self.base_link, search_link % (quote_plus(query)))
+			log_utils.log('url = %s' % url)
 			html = client.request(url, timeout=7)
 
 			# if not html or '<tbody' not in html: return sources
@@ -57,7 +60,6 @@ class source:
 			table = client.parseDOM(html, 'div', attrs={'id': 'index'})
 
 			rows = client.parseDOM(table, 'tr')
-			# log_utils.log('rows = %s' % rows)
 			undesirables = source_utils.get_undesirables()
 			check_foreign_audio = source_utils.check_foreign_audio()
 		except:
@@ -70,19 +72,15 @@ class source:
 				columns = re.findall(r'<td.*?>(.*?)</td>', row, re.DOTALL)
 
 				url = re.search(r'href\s*=\s*["\'](magnet:.+?)["\']', columns[1], re.I).group(1)
-
 				url = unquote_plus(url).replace('&amp;', '&').replace('&#x3D;', '=').split('&tr')[0]
 				url = url.replace('&dn=rutor.info', '&dn=[rutor.info]')
-				# log_utils.log('url = %s' % url)
-
+				# url = unquote_plus(client.replaceHTMLCodes(link[0])).split('&tr')[0]
+				# url = re.sub(r'(&tr=.+)&dn=', '&dn=', url).replace(' ', '.') # some links on bitsearch &tr= before &dn=
 				hash = re.search(r'btih:(.*?)&', url, re.I).group(1)
-				# log_utils.log('hash = %s' % hash)
 
 				name = re.search(r'/torrent/.+?["\']>(.+?)<', columns[1], re.I).group(1)
-				name = source_utils.clean_name(name).replace('2./.', '').replace('./.', '').replace('|.', '').replace('...', '.').replace('..', '.').replace(',.', '.')
+				name = source_utils.clean_name(name).replace('2./.', '').replace('./.', '').replace('|.', '').replace('...', '.').replace('..', '.').replace(',.', '.').replace(':.', '.')
 				url = '%s%s' % (url, name)
-				# log_utils.log('name = %s' % name)
-				# log_utils.log('url = %s' % url)
 
 				if not source_utils.check_title(title, aliases, name, hdlr, year):
 					continue
@@ -92,30 +90,30 @@ class source:
 				if undesirables and source_utils.remove_undesirables(name_info, undesirables):
 					continue
 
-				if not episode_title: #filter for eps returned in movie query (rare but movie and show exists for Run in 2020)
+				if not episode_title: # filter for eps returned in movie query (rare but movie and show exists for Run in 2020)
 					ep_strings = [r'[.-]s\d{2}e\d{2}([.-]?)', r'[.-]s\d{2}([.-]?)', r'[.-]season[.-]?\d{1,2}[.-]?']
 					name_lower = name.lower()
 					if any(re.search(item, name_lower) for item in ep_strings): continue
 
 				try:
-					seeders = re.search(r'>([0-9]+|[0-9]+,[0-9]+|[0-9]+\.[0-9]+K)<', columns[2], re.S | re.I).group(1)
-					# log_utils.log('seeders = %s' % seeders)
+					seeders = client.parseDOM(columns[-1], 'span', attrs={'class': 'green'}) # some results have comments so seeders is always last col from end
+					seeders = re.search(r'/>(.+)', seeders[0], re.I).group(1).replace('&nbsp;', ' ')
 					if 'K' in seeders: seeders = float(seeders.rstrip('K')) * 1000
 					seeders = int(seeders)
 					if self.min_seeders > seeders: continue
-				except: seeders = 0
+				except:
+					seeders = 0
 
 				quality, info = source_utils.get_release_quality(name_info, url)
 				try:
 					size = columns[-2].replace('&nbsp;', ' ') # some results have comments so size is always 2nd col from end
-					# log_utils.log('size = %s' % size)
 					dsize, isize = source_utils._size(size)
 					info.insert(0, isize)
 				except: dsize = 0
 				info = ' | '.join(info)
 
 				append({'provider': 'rutor', 'source': 'torrent', 'seeders': seeders, 'hash': hash, 'name': name, 'name_info': name_info,
-												'quality': quality, 'language': 'en', 'url': url, 'info': info, 'direct': False, 'debridonly': True, 'size': dsize})
+										'quality': quality, 'language': 'en', 'url': url, 'info': info, 'direct': False, 'debridonly': True, 'size': dsize})
 			except:
 				source_utils.scraper_error('RUTOR')
 		return sources
@@ -141,12 +139,13 @@ class source:
 			query = re.sub(r'[^A-Za-z0-9\s\.-]+', '', self.title)
 			if search_series:
 				queries = [
-						self.search_link % ('0/4/110/8', quote_plus(query + ' Season')),
-						self.search_link % ('0/4/110/8', quote_plus(query + ' Complete'))]
+						self.search_link % (self.tvshow_params, quote_plus(query + ' Season')),
+						self.search_link % (self.tvshow_params, quote_plus(query + ' Complete'))]
 			else:
 				queries = [
-						self.search_link % ('0/4/110/8', quote_plus(query + ' S%s' % self.season_xx)),
-						self.search_link % ('0/4/110/8', quote_plus(query + ' Season %s' % self.season_x))]
+						self.search_link % (self.tvshow_params, quote_plus(query + ' S%s' % self.season_xx)),
+						self.search_link % (self.tvshow_params, quote_plus(query + ' Season %s' % self.season_x))]
+
 			threads = []
 			append = threads.append
 			for url in queries:
@@ -161,30 +160,30 @@ class source:
 
 	def get_sources_packs(self, link):
 		try:
+			log_utils.log('link = %s' % link)
 			results = client.request(link, timeout=7)
+			# if not html or '<tbody' not in html: return sources
 			if not results or '<div id="index">' not in results: return sources
+			# table = client.parseDOM(html, 'tbody')
 			table = client.parseDOM(results, 'div', attrs={'id': 'index'})
 			rows = client.parseDOM(table, 'tr')
 		except:
 			source_utils.scraper_error('RUTOR')
-			return
 		for row in rows:
 			try:
 				if 'magnet:' not in row: continue
 				columns = re.findall(r'<td.*?>(.*?)</td>', row, re.DOTALL)
-				
+
 				url = re.search(r'href\s*=\s*["\'](magnet:.+?)["\']', columns[1], re.I).group(1)
 				url = unquote_plus(url).replace('&amp;', '&').replace('&#x3D;', '=').split('&tr')[0]
 				url = url.replace('&dn=rutor.info', '&dn=[rutor.info]')
-				
+				# url = unquote_plus(client.replaceHTMLCodes(link[0])).split('&tr')[0]
+				# url = re.sub(r'(&tr=.+)&dn=', '&dn=', url).replace(' ', '.') # some links on bitsearch &tr= before &dn=
 				hash = re.search(r'btih:(.*?)&', url, re.I).group(1)
-				# log_utils.log('hash = %s' % hash)
 
 				name = re.search(r'/torrent/.+?["\']>(.+?)<', columns[1], re.I).group(1)
-				name = source_utils.clean_name(name).replace('2./.', '').replace('./.', '').replace('|.', '').replace('...', '.').replace('..', '.').replace(',.', '.')
+				name = source_utils.clean_name(name).replace('2./.', '').replace('./.', '').replace('|.', '').replace('...', '.').replace('..', '.').replace(',.', '.').replace(':.', '.')
 				url = '%s%s' % (url, name)
-				# log_utils.log('name = %s' % name)
-				# log_utils.log('url = %s' % url)
 
 				episode_start, episode_end = 0, 0
 				if not self.search_series:
@@ -205,8 +204,8 @@ class source:
 				if self.undesirables and source_utils.remove_undesirables(name_info, self.undesirables): continue
 
 				try:
-					seeders = re.search(r'>([0-9]+|[0-9]+,[0-9]+|[0-9]+\.[0-9]+K)<', columns[2], re.S | re.I).group(1)
-					# log_utils.log('seeders = %s' % seeders)
+					seeders = client.parseDOM(columns[-1], 'span', attrs={'class': 'green'}) # some results have comments so seeders is always last col from end
+					seeders = re.search(r'/>(.+)', seeders[0], re.I).group(1).replace('&nbsp;', ' ')
 					if 'K' in seeders: seeders = float(seeders.rstrip('K')) * 1000
 					seeders = int(seeders)
 					if self.min_seeders > seeders: continue
@@ -215,7 +214,6 @@ class source:
 				quality, info = source_utils.get_release_quality(name_info, url)
 				try:
 					size = columns[-2].replace('&nbsp;', ' ') # some results have comments so size is always 2nd col from end
-					# log_utils.log('size = %s' % size)
 					dsize, isize = source_utils._size(size)
 					info.insert(0, isize)
 				except: dsize = 0
