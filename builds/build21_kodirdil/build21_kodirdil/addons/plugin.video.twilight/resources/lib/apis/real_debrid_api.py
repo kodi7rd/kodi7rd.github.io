c3a05fb59df4e1e686fad0393d60f4fd19c08908
kodi7rd <KodiRealDebridIsrael@github.com>
2024-11-25 15:00:16 +0200

    KODI-RD-IL | Twilight 3.2.80

--- builds/build21_kodirdil/build21_kodirdil/addons/plugin.video.twilight/resources/lib/apis/real_debrid_api.py
+++ builds/build21_kodirdil/build21_kodirdil/addons/plugin.video.twilight/resources/lib/apis/real_debrid_api.py
@@ -252,9 +252,9 @@ class RealDebridAPI:
 			if not transfer_finished:
 				self.delete_torrent(torrent_id)
 				return None
-			selected_files = [(idx, i) for idx, i in enumerate([i for i in torrent_info['files'] if i['selected'] == 1])]
-			match = False
+			selected_files = [(idx, i) for idx, i in enumerate([i for i in torrent_info['files'] if i['selected'] == 1 and i['path'].lower().endswith(tuple(extensions))])]
 			selected_files = sorted(selected_files, key=lambda x: x[1]['bytes'], reverse=True)
+			match = False
 			if season:
 				correct_files = []
 				correct_file_check = False
@@ -270,7 +270,6 @@ class RealDebridAPI:
 						else: match = True; break
 				if match: index = [i[0] for i in selected_files if i[1]['path'] == correct_files[0]['path']][0]
 			else:
-				selected_files = sorted(selected_files, key=lambda x: x[1]['bytes'], reverse=True)
 				for value in selected_files:
 					filename = re.sub(r'[^A-Za-z0-9-]+', '.', value[1]['path'].rsplit('/', 1)[1].replace('\'', '').replace('&', 'and').replace('%', '.percent')).lower()
 					filename_info = filename.replace(compare_title, '')
@@ -360,26 +359,25 @@ class RealDebridAPI:
 		# 	return None
 
 	def display_magnet_pack(self, magnet_url, info_hash):
-		from modules.source_utils import supported_video_extensions
 		try:
-			video_only_items, list_file_items = [], []
-			video_only_append = video_only_items.append
-			extensions = supported_video_extensions()
-			torrent_files = self.check_hash(info_hash)
-			if not info_hash in torrent_files: return None
 			torrent = self.add_magnet(magnet_url)
 			torrent_id = torrent['id']
-			torrent_files = torrent_files[info_hash]['rd']
-			torrent_files = [item for item in torrent_files if self.video_only(item, extensions)]
-			for item in torrent_files:
-				torrent_keys = item.keys()
-				if len(torrent_keys) == 0: continue
-				video_only_append(torrent_keys)
-			if not video_only_items: return None
-			video_only_items = max(video_only_items, key=len)
-			torrent_keys = ','.join(video_only_items)
-			self.add_torrent_select(torrent_id, torrent_keys)
-			torrent_info = self.user_cloud_info(torrent_id)
+			self.add_torrent_select(torrent_id, 'all')
+			torrent_info = self.user_cloud_info_check(torrent_id)
+			if not torrent_info['links'] or 'error' in torrent_info:
+				self.delete_torrent(torrent_id)
+				return None
+			sleep(1000)
+			elapsed_time, transfer_finished = 0, False
+			while elapsed_time <= 7 and not transfer_finished:
+				active_count = self.torrents_activeCount()
+				active_list = active_count['list']
+				elapsed_time += 1
+				if info_hash in active_list: sleep(1000)
+				else: transfer_finished = True
+			if not transfer_finished:
+				self.delete_torrent(torrent_id)
+				return None
 			list_file_items = [dict(i, **{'link': torrent_info['links'][idx]}) for idx, i in enumerate([i for i in torrent_info['files'] if i['selected'] == 1])]
 			list_file_items = [{'link': i['link'], 'filename': i['path'].replace('/', ''), 'size': i['bytes']} for i in list_file_items]
 			self.delete_torrent(torrent_id)
